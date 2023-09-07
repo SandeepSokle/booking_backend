eb453a71ecc93d75e7baf5aea92742f86fcc4c3c
Sandeep Sokle <sandeepsokle12@gmail.com>
2023-09-07 09:26:03 +0530

    add get booking data API

--- src/controller/bookingController.js
+++ src/controller/bookingController.js
@@ -1,11 +1,14 @@
 const bookingsModel = require("../Models/bookingModel");
-const users = require("../Models/bookingModel");
 
 const getbookings = async (req, res, next) => {
   // console.log(req.body);
   try {
+    const booking = await bookingsModel.find();
+
     res.status(200).send({
       msg: "send Successfully!",
+      count: booking.length,
+      booking,
     });
   } catch (err) {
     console.log(err.message);
@@ -13,7 +16,6 @@ const getbookings = async (req, res, next) => {
 };
 
 const addbookings = async (req, res, next) => {
-  // console.log(req.body);
   try {
     const booking = await bookingsModel.create({
       ...req.body,
